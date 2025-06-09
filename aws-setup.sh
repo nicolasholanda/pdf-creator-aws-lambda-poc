79ea69c3d02bdd21888e9200594d2b205e683d6b
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-06-09 10:55:22 -0300

    Fixed some problems from lambda logs

---
+++ aws-setup.sh
@@ -0,0 +1,2 @@
+aws lambda create-function --function-name pdf-lambda --runtime java17 --handler com.github.nicolasholanda.PdfLambdaHandler::handleRequest --role arn:aws:iam::000000000000:role/lambda-role --zip-file fileb://target/pdf-creator-aws-lambda-poc-1.0-SNAPSHOT.jar
+aws lambda create-event-source-mapping --function-name pdf-lambda --event-source-arn arn:aws:sqs:us-east-1:000000000000:pdf-generation
