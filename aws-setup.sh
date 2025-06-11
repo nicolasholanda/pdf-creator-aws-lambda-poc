dc02a9834583a43e0517a513fa55dd860f3e9dcf
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-06-10 22:26:03 -0300

    Added setup for localstack

--- aws-setup.sh
+++ aws-setup.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+aws sqs create-queue --queue-name pdf-generation
 aws lambda create-function --function-name pdf-lambda --runtime java17 --handler com.github.nicolasholanda.PdfLambdaHandler::handleRequest --role arn:aws:iam::000000000000:role/lambda-role --zip-file fileb://target/pdf-creator-aws-lambda-poc-1.0-SNAPSHOT.jar
 aws lambda create-event-source-mapping --function-name pdf-lambda --event-source-arn arn:aws:sqs:us-east-1:000000000000:pdf-generation
+aws s3api create-bucket --bucket pdf-bucket
